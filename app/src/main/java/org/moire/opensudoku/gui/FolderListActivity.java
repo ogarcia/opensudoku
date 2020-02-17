06b5a8161cd02e1f8af01346c02352333a05e65e
Justin Nordin <jlnordin@gmail.com>
2020-02-17 11:23:13 -0800

    Add option to skip the titelscreen and launch the sudoku lists activity immediately on launch.

--- app/src/main/java/org/moire/opensudoku/gui/FolderListActivity.java
+++ app/src/main/java/org/moire/opensudoku/gui/FolderListActivity.java
@@ -129,6 +129,10 @@ public class FolderListActivity extends ThemedActivity {
             }
         });
         registerForContextMenu(mListView);
+
+        // show changelog on first run
+        Changelog changelog = new Changelog(this);
+        changelog.showOnFirstRun();
     }
 
     @Override
